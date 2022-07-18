3ccee73b718544f9704c4591f739f0b1bc9abb7f
cpt arduous <mohitneg377@gmail.com>
2022-07-18 09:00:44 +0530

    wave print completed

--- 02Array2d/wavePrint.cpp
+++ 02Array2d/wavePrint.cpp
@@ -1,59 +1,48 @@
 #include<iostream>
 
 int main()
-{   
+{
     {
         using namespace std;
         {
-            cout<<"Enter the size of the 2D array ";
-            cout<<"\nfor rows :";
-            int m{};
-            cin>>m;
-
-            cout<<"\n for columns :";
-            int n{};
-            cin>>n;
-
             int arr[1000][1000];
-
-            //for filling up the array
+            
+            int m{},n{};
+            cin>>m>>n;
+            
             int temp{};
-
-            for(int row{};row<=m-1;row++)
+            for(int row{};row<m;row++)
             {
-                for(int col{};col<=n-1;col++)
+                for(int col{};col<n;col++)
                 {
                     temp++;
                     arr[row][col] = temp;
-                    cout<<arr[row][col]<<'\t';
-                }cout<<'\n';
+                    //cout<<arr[row][col]<<'\t';
+                }
+                //cout<<'\n';
             }
             
-            //for wave print
-            
-            for(int col{};col<n;col++)
+            //wave print
+            for(int row{};row<m;row++)
             {
-                if(col % 2 == 0)
-                {
-                    for(int row{};row<m;row++)
-                    {
-                        cout<<arr[row][col]<<" ";
-                    }
-                    
-                }
-                else
-                {
-                    for(int row{m-1};row>=0;row--)
-                    {
-                        cout<<arr[row][col]<<" ";
-                    }
-                }
-                cout<<'\n';
+               if(row % 2 == 0)
+               {
+                   for(int col{n-1};col>=0;col--)
+                   {
+                       cout<<arr[row][col]<<'\t';
+                   }
+               }else
+               {
+                   for(int col{};col<n;col++)
+                   {
+                       cout<<arr[row][col]<<'\t';
+                   }
+               }
+               cout<<'\n';
+               
             }
-
         } // namespace std;
         
     }
-
     return 0;
 }
